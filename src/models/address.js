5bfdb5d5731435baaa550883d73415f3d2bdfe93
dabperceptive <daniel.bedarf@gmail.com>
2016-12-06 17:46:03 +0100

    address implemented

--- src/models/address.js
+++ src/models/address.js
@@ -1,18 +1,117 @@
 'use strict';
 
-var Artefact = require('./artefact');
+function Address(json) {
 
-//the build model
-function Build(nummer, comment, scmId, timestamp, artefacts) {
-    this.nummer = nummer;
-    this.comment = comment;
-    this.timestamp = timestamp || Date.now().toString();
-    this.scmId = scmId;
+
+    var _internalData = {
+        country: '',
+        city: '',
+        state: '',
+        zipcode: '',
+        street: '',
+        street2: '',
+        geo: {
+            lat: '',
+            lng: ''
+        }
+    };
+
+    if (json !== undefined) {
+        _internalData.country = json.country || _internalData.country;
+        _internalData.city = json.city || _internalData.city;
+        _internalData.state = json.state || _internalData.state;
+        _internalData.zipcode = json.zipcode || _internalData.zipcode;
+        _internalData.street = json.street || _internalData.street;
+        _internalData.street2 = json.street2 || _internalData.street2;
+        _internalData.geo = json.geo || _internalData.geo;
+    }
+
+
+
+    //we encapsulate all properties so that we can add validations
+    Object.defineProperty(this, 'country', {
+        get: function() {
+            return _internalData.country;
+        },
+        set: function(value) {
+            if (value !== undefined) {
+                _internalData.country = value;
+            } else {
+                _internalData.country = '';
+            }
+        }
+    });
+
+    Object.defineProperty(this, 'city', {
+        get: function() {
+            return _internalData.city;
+        },
+        set: function(value) {
+            if (value !== undefined) {
+                _internalData.city = value;
+            } else {
+                _internalData.city = '';
+            }
+        }
+    });
+
+    Object.defineProperty(this, 'zipcode', {
+        get: function() {
+            return _internalData.zipcode;
+        },
+        set: function(value) {
+            if (value !== undefined) {
+                _internalData.zipcode = value;
+            } else {
+                _internalData.zipcode = '';
+            }
+        }
+    });
+
+
+    Object.defineProperty(this, 'street', {
+        get: function() {
+            return _internalData.street;
+        },
+        set: function(value) {
+            if (value !== undefined) {
+                _internalData.street = value;
+            } else {
+                _internalData.street = '';
+            }
+        }
+    });
+
+    Object.defineProperty(this, 'street2', {
+        get: function() {
+            return _internalData.street2;
+        },
+        set: function(value) {
+            if (value !== undefined) {
+                _internalData.street2 = value;
+            } else {
+                _internalData.street2 = '';
+            }
+        }
+    });
+
+    Object.defineProperty(this, 'geo', {
+        get: function() {
+            return _internalData.geo;
+        },
+        set: function(value) {
+            if (value !== undefined) {
+                _internalData.geo = value;
+            } else {
+                _internalData.geo = '';
+            }
+        }
+    });
+
 }
 
-Build.prototype.addArtefact = function (artefact) {
-    if (!(artefact instanceof Artefact)) throw new Error('paramater artefact is not an instance of Artefact');
-    if (!this.artefacts) this.artefacts = [];
-    this.artefacts.push(artefact);
+Address.prototype.toJson = function() {
+    return JSON.stringify(this._internalData, null, 2);
 };
-module.exports = Build;
+
+module.exports = Address;
