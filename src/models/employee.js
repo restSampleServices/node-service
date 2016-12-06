90989d50889c75c60be7fa16e3bf7ab45cf9a9ae
dabperceptive <daniel.bedarf@gmail.com>
2016-12-06 17:36:33 +0100

    employee model finished

--- src/models/employee.js
+++ src/models/employee.js
@@ -1,6 +1,6 @@
 'use strict';
-var company = require('./company');
-var address = require('./address');
+var Company = require('./company');
+var Address = require('./address');
 
 function Employee(json) {
 
@@ -9,23 +9,43 @@ function Employee(json) {
         firstName: '',
         lastName: '',
         userName: '',
-        email: ''
+        email: '',
+        jobTitle: '',
+        department: '',
+        imageUrl: undefined,
+        phone: '',
+        dateOfBirth: undefined,
+        address: undefined,
+        jobHistory: []
     };
+
     if (json !== undefined) {
         _internalData.firstName = json.firstName || _internalData.firstName;
         _internalData.lastName = json.lastName || _internalData.lastName;
         _internalData.userName = json.userName || _internalData.userName;
         _internalData.email = json.email || _internalData.email;
+        _internalData.jobTitle = json.jobTitle || _internalData.jobTitle;
+        _internalData.department = json.department || _internalData.department;
+        _internalData.imageUrl = json.imageUrl || _internalData.imageUrl;
+        _internalData.phone = json.phone || _internalData.phone;
+        _internalData.dateOfBirth = json.dateOfBirth || _internalData.dateOfBirth;
+        if (json.address !== undefined) {
+            _internalData.address = new Address(json.address);
+        }
+
+        if (json.jobHistory !== undefined) {
+            _internalData.jobHistory = Company.createList(json.jobHistory);
+        }
     }
 
 
 
     //we encapsulate all properties so that we can add validations
     Object.defineProperty(this, 'firstName', {
-        get: function () {
+        get: function() {
             return _internalData.firstName;
         },
-        set: function (value) {
+        set: function(value) {
             if (value !== undefined) {
                 _internalData.firstName = value;
             } else {
@@ -35,10 +55,10 @@ function Employee(json) {
     });
 
     Object.defineProperty(this, 'lastName', {
-        get: function () {
+        get: function() {
             return _internalData.lastName;
         },
-        set: function (value) {
+        set: function(value) {
             if (value !== undefined) {
                 _internalData.lastName = value;
             } else {
@@ -48,13 +68,114 @@ function Employee(json) {
     });
 
     Object.defineProperty(this, 'fullName', {
-        get: function () {
+        get: function() {
             return _internalData.firstName + ' ' + _internalData.lastName;
         }
     });
+
+    Object.defineProperty(this, 'dateOfBirth', {
+        get: function() {
+            return _internalData.dateOfBirth;
+        },
+        set: function(value) {
+            if (value !== undefined) {
+                _internalData.dateOfBirth = value;
+            } else {
+                _internalData.dateOfBirth = '';
+            }
+        }
+    });
+
+    Object.defineProperty(this, 'email', {
+        get: function() {
+            return _internalData.email;
+        },
+        set: function(value) {
+            if (value !== undefined) {
+                //TODO validate at sign etc
+                _internalData.email = value;
+            } else {
+                _internalData.email = '';
+            }
+        }
+    });
+
+    Object.defineProperty(this, 'phone', {
+        get: function() {
+            return _internalData.phone;
+        },
+        set: function(value) {
+            if (value !== undefined) {
+                _internalData.phone = value;
+            } else {
+                _internalData.phone = '';
+            }
+        }
+    });
+
+    Object.defineProperty(this, 'address', {
+        get: function() {
+            return _internalData.address;
+        },
+        set: function(value) {
+            if (1 != 2) {
+                //TODO check for typeof Address
+                _internalData.address = value;
+            } else {
+                throw new Error('invalid data type for address');
+            }
+        }
+    });
+
+    Object.defineProperty(this, 'imageUrl', {
+        get: function() {
+            return _internalData.imageUrl;
+        },
+        set: function(value) {
+            if (value !== undefined) {
+                _internalData.imageUrl = value;
+            } else {
+                _internalData.imageUrl = '';
+            }
+        }
+    });
+
+
+    Object.defineProperty(this, 'department', {
+        get: function() {
+            return _internalData.department;
+        },
+        set: function(value) {
+            if (value !== undefined) {
+                _internalData.department = value;
+            } else {
+                _internalData.department = '';
+            }
+        }
+    });
+
+    Object.defineProperty(this, 'jobTitle', {
+        get: function() {
+            return _internalData.jobTitle;
+        },
+        set: function(value) {
+            if (value !== undefined) {
+                _internalData.jobTitle = value;
+            } else {
+                _internalData.jobTitle = '';
+            }
+        }
+    });
+
+    Object.defineProperty(this, 'jobHistory', {
+        get: function() {
+            return _internalData.jobHistory;
+        }
+    });
+
 }
 
-Employee.prototype.toJson = function () {
+Employee.prototype.toJson = function() {
     return JSON.stringify(this._internalData, null, 2);
 };
 
