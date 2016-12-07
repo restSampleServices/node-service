a85f7477a984fcf29fde894aac6885c4fd37c09d
dabperceptive <daniel.bedarf@gmail.com>
2016-12-07 14:30:57 +0100

    add dto for employee collections

---
+++ src/rest/dto/employeeCollectionEntry.js
@@ -0,0 +1,23 @@
+'use strict';
+
+//this object is used when a collection of employees is returned, to reduce the transported data
+function EmployeeCollectionEntry(employee) {
+    //TODO check that employee is based on Employee Class
+    var _employee = employee;
+    Object.defineProperty(this, 'firstName', {
+        enumerable: true,
+        value: _employee.firstName
+    });
+
+    Object.defineProperty(this, 'lastName', {
+        enumerable: true,
+        value: _employee.lastName
+    });
+
+    Object.defineProperty(this, 'userName', {
+        enumerable: true,
+        value: _employee.userName
+    });
+}
+
+module.exports = EmployeeCollectionEntry;;
--- src/rest/employeesController.js
+++ src/rest/employeesController.js
@@ -4,19 +4,24 @@ var endpoints = require('./endpoints.js');
 var errorHandler = require('../errorHandler.js');
 
 var employeeDB = require('./../persistence/employeePersistence.js');
+var EmployeeCollectionEntry = require('./dto/employeeCollectionEntry');
 
 function getEmployees(req, res) {
     try {
-        log.info('employeeController.getemployee ');
-        employeeDB.getAllEmployees().then(function (employees) {
-            log.debug(JSON.stringify(employees, null, 2));
-            res.json(employees);
-        }).catch(function (dbError) {
+        log.info('employeeController.getEmployees');
+        employeeDB.getAllEmployees().then(function(employees) {
+            var dto = [];
+            //reduce the amount of transported data
+            employees.forEach(function(employee) {
+                dto.push(new EmployeeCollectionEntry(employee));
+            })
+            res.json(dto);
+        }).catch(function(dbError) {
             errorHandler.InternalServerError(dbError, res);
         });
 
     } catch (err) {
-        log.debug('error in employeeController.getemployee');
+        log.error('error in employeeController.getemployee');
         errorHandler.InternalServerError(err, res);
     }
 }
@@ -24,10 +29,10 @@ function getEmployees(req, res) {
 function getEmployeeById(req, res) {
     try {
         log.info('employeeController.getemployee ');
-        employeeDB.getAllEmployees().then(function (employees) {
+        employeeDB.getAllEmployees().then(function(employees) {
             log.verbose(employees);
             res.json(employees);
-        }).catch(function (dbError) {
+        }).catch(function(dbError) {
             errorHandler.DataNotFound(dbError, res);
         });
 
