3f360f7443b29729327345ddb120eb0f688a2a63
dabperceptive <daniel.bedarf@gmail.com>
2016-12-07 17:46:46 +0100

    fix unit tests

--- test/rest/errorHandlerTest.js
+++ test/rest/errorHandlerTest.js
@@ -4,14 +4,14 @@ var assert = chai.assert; //http://chaijs.com/api/assert/
 var expect = chai.expect; //http://chaijs.com/api/bdd/#method_language-chains
 
 var httpMocks = require('node-mocks-http'); //doc: https://github.com/howardabrams/node-mocks-http
-var errorHandler = require('../../src/errorHandler.js');
+var errorHandler = require('../../src/rest/errorHandler.js');
 
-describe('rest', function () {
+describe('rest', function() {
 
     var errorPages = {};
-    before(function (done) {
+    before(function(done) {
         var fs = require('fs');
-        fs.readFile(__dirname + '/../../src/html/404.html', 'utf8', function (err, html) {
+        fs.readFile(__dirname + '/../../src/html/404.html', 'utf8', function(err, html) {
             if (err) {
                 errorPages.P404 = '';
                 throw err;
@@ -23,24 +23,24 @@ describe('rest', function () {
     });
 
     describe('errorHandler',
-        function () {
-            describe('InternalServerError', function () {
-                it('is defined', function () {
+        function() {
+            describe('InternalServerError', function() {
+                it('is defined', function() {
                     //used for filesystem operation etc
                     assert.isDefined(errorHandler.InternalServerError);
                     assert.isFunction(errorHandler.InternalServerError);
                 });
 
-                it('throws error if response parametr is empty', function () {
-                    var fail = function () {
+                it('throws error if response parametr is empty', function() {
+                    var fail = function() {
                         errorHandler.InternalServerError(Error('lorem ipsum'), null);
                     };
 
                     assert.throws(fail, Error);
                 });
 
-                it('should not fail with a defect response parameter', function () {
-                    var fail = function () {
+                it('should not fail with a defect response parameter', function() {
+                    var fail = function() {
                         errorHandler.InternalServerError(Error('lorem ipsum'), {
                             'some': 'data'
                         });
@@ -48,8 +48,8 @@ describe('rest', function () {
                     expect(fail).to.not.throw(Error);
                 });
 
-                it('do fail if err parameter is missing', function () {
-                    var fail = function () {
+                it('do fail if err parameter is missing', function() {
+                    var fail = function() {
                         var response = httpMocks.createResponse();
                         errorHandler.InternalServerError(null, response);
                     };
@@ -57,36 +57,36 @@ describe('rest', function () {
                     expect(fail).to.throw(Error);
                 });
 
-                it('sending response with errorcode 500', function () {
+                it('sending response with errorcode 500', function() {
                     var response = httpMocks.createResponse();
                     errorHandler.InternalServerError(Error('lorem ipsum'), response);
                     expect(response.statusCode).to.equal(500);
                 });
 
-                it('sending response with defined error message ', function () {
+                it('sending response with defined error message ', function() {
                     var response = httpMocks.createResponse();
                     errorHandler.InternalServerError(Error('lorem ipsum'), response);
                     expect(response._getData()).to.include('lorem ipsum');
                 });
             }); //500
 
-            describe('DataNotFound', function () {
-                it('is defined', function () {
+            describe('DataNotFound', function() {
+                it('is defined', function() {
                     //used for filesystem operation etc
                     assert.isDefined(errorHandler.DataNotFound);
                     assert.isFunction(errorHandler.DataNotFound);
                 });
 
-                it('do fail if response parameter is empty', function () {
-                    var fail = function () {
+                it('do fail if response parameter is empty', function() {
+                    var fail = function() {
                         errorHandler.DataNotFound(Error('lorem ipsum'), null);
                     };
                     assert.throws(fail, Error);
                     expect(fail).to.throw(Error);
                 });
 
-                it('do fail if err parameter is missing', function () {
-                    var fail = function () {
+                it('do fail if err parameter is missing', function() {
+                    var fail = function() {
                         var response = httpMocks.createResponse();
                         errorHandler.DataNotFound(null, response);
                     };
@@ -94,7 +94,7 @@ describe('rest', function () {
                     expect(fail).to.throw(Error);
                 });
 
-                it('sending response with errorcode 404', function (done) {
+                it('sending response with errorcode 404', function(done) {
 
                     var response = httpMocks.createResponse({
                         eventEmitter: require('events').EventEmitter
@@ -113,7 +113,7 @@ describe('rest', function () {
                     }, 1000);*/
                 });
 
-                it('sending response is the 404.html', function (done) {
+                it('sending response is the 404.html', function(done) {
                     var response = httpMocks.createResponse({
                         eventEmitter: require('events').EventEmitter
                     });
