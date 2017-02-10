7fbebd497dba02048b0d5062e9e8777fd4c8627c
Daniel <github@blndev.com>
2017-02-10 20:14:16 +0100

    finalize job model test

--- test/models/jobModelTest.js
+++ test/models/jobModelTest.js
@@ -15,110 +15,116 @@ describe('Models', function () {
 
     });
     
-    describe('JOb',
+    describe('Job',
         function () {
             it('initialization and getter for address works', function () {
                 var testData = new Address();
                 var job = new Job({address: testData});
-                assert.isString(job.address);
+                assert.isObject(job.address);
                 assert.equal(job.address, testData);
             });
         
-            it('setter and getter for country works', function () {
-                var testData = 'simple test value';
+            it('setter and getter for address works', function () {
+                var testData = new Address();
                 var job = new Job();
-                assert.isString(job.country);
-                assert.notEqual(job.country, testData);
-                job.country= testData;
-                assert.equal(job.country, testData);
+                assert.isUndefined(job.address);
+                assert.notEqual(job.address, testData);
+                job.address= testData;
+                assert.equal(job.address, testData);
             });
 
-            it('initialization and getter for city works', function () {
-                var testData = 'simple test value';
-                var job = new Job({city: testData});
-                assert.isString(job.city);
-                assert.equal(job.city, testData);
-            });
-        
-            it('setter and getter for city works', function () {
-                var testData = 'simple test value';
+            it('setter and getter for address works', function () {
                 var job = new Job();
-                assert.isString(job.city);
-                assert.notEqual(job.city, testData);
-                job.city= testData;
-                assert.equal(job.city, testData);
+                assert.throws(function(){
+                  job.address = new Date();
+                });
+            });
+    
+            it('initialization and getter for id works', function () {
+                var testData = '1337';
+                var job = new Job({id: testData});
+                assert.isString(job.id);
+                assert.equal(job.id, testData);
             });
         
-            it('initialization and getter for zipcode works', function () {
+            it('setter for id is forbidden', function () {
+                var testData = '1337';
+                var job = new Job();
+                assert.isString(job.id);
+                assert.notEqual(job.id, testData);
+                assert.throws(function(){
+                  job.id = testData;
+                });
+            });
+        
+            it('initialization and getter for companyName works', function () {
                 var testData = '12345';
-                var job = new Job({zipcode: testData});
-                assert.isString(job.zipcode);
-                assert.equal(job.zipcode, testData);
+                var job = new Job({companyName: testData});
+                assert.isString(job.companyName);
+                assert.equal(job.companyName, testData);
             });
         
-            it('setter and getter for zipcode works', function () {
+            it('setter and getter for companyName works', function () {
                 var testData = 'simple test value';
                 var job = new Job();
-                assert.isString(job.zipcode);
-                assert.notEqual(job.zipcode, testData);
-                job.zipcode = testData;
-                assert.equal(job.zipcode, testData);
+                assert.isString(job.companyName);
+                assert.notEqual(job.companyName, testData);
+                job.companyName = testData;
+                assert.equal(job.companyName, testData);
+                job.companyName= undefined;
+                assert.equal(job.companyName, '');
             });
 
-            it('initialization and getter for street works', function () {
+            it('initialization and getter for department works', function () {
                 var testData = 'simple test value';
-                var job = new Job({street: testData});
-                assert.isString(job.street);
-                assert.equal(job.street, testData);
+                var job = new Job({department: testData});
+                assert.isString(job.department);
+                assert.equal(job.department, testData);
             });
         
-            it('setter and getter for street works', function () {
-                var testData = 'simple test value';
-                var job = new Job();
-                assert.isString(job.street);
-                assert.notEqual(job.street, testData);
-                job.street = testData;
-                assert.equal(job.street, testData);
-            });
-        
-            it('initialization and getter for street2 works', function () {
-                var testData = 'simple test value';
-                var job = new Job({street2: testData});
-                assert.isString(job.street2);
-                assert.equal(job.street2, testData);
-            });
-        
-            it('setter and getter for street2 works', function () {
+            it('setter and getter for department works', function () {
                 var testData = 'simple test value';
                 var job = new Job();
-                assert.isString(job.street2);
-                assert.notEqual(job.street2, testData);
-                job.street2 = testData;
-                assert.equal(job.street2, testData);
+                assert.isString(job.department);
+                assert.notEqual(job.department, testData);
+                job.department = testData;
+                assert.equal(job.department, testData);
+                job.department = undefined;
+                assert.equal(job.department, '');
             });
         
-            it('initialization and getter for geo works', function () {
-                var testData = {latitude: '1234', longitude: '9876'};
-                var job = new Job({geo: testData});
-                assert.isObject(job.geo);
-                assert.equal(job.geo.latitude, testData.latitude);
-                assert.equal(job.geo.longitude, testData.longitude);
+            it('initialization and getter for dateStart works', function () {
+                var testData = 'simple test value';
+                var job = new Job({dateStart: testData});
+                assert.isString(job.dateStart);
+                assert.equal(job.dateStart, testData);
             });
         
-            it('setter and getter for geo works', function () {
-                var testData = {latitude: '1234', longitude: '9876'};
+            it('setter and getter for dateStart works', function () {
+                var testData = 'simple test value';
                 var job = new Job();
-                assert.isObject(job.geo);
-                assert.isString(job.geo.latitude);
-                assert.isString(job.geo.longitude);
-
-                assert.notEqual(job.geo.latitude, testData.latitude);
-                assert.notEqual(job.geo.longitude, testData.longitude);
-
-                job.geo = testData;
-
-                assert.equal(job.geo.latitude, testData.latitude);
-                assert.equal(job.geo.longitude, testData.longitude);
+                assert.isString(job.dateStart);
+                assert.notEqual(job.dateStart, testData);
+                job.dateStart = testData;
+                assert.equal(job.dateStart, testData);
+                job.dateStart = undefined;
+                assert.equal(job.dateStart, '');
+            });
+        
+            it('initialization and getter for dateEnd works', function () {
+                var testData = 'simple test value';
+                var job = new Job({dateEnd: testData});
+                assert.isString(job.dateEnd);
+                assert.equal(job.dateEnd, testData);
+            });
+        
+            it('setter and getter for dateEnd works', function () {
+                var testData = 'simple test value';
+                var job = new Job();
+                assert.isString(job.dateEnd);
+                assert.notEqual(job.dateEnd, testData);
+                job.dateEnd = testData;
+                assert.equal(job.dateEnd, testData);
             });
 
         });
