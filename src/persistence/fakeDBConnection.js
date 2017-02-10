c59a0ba3fc67d1ceabf19abaa377d6948e5e73f5
Daniel <github@blndev.com>
2017-02-10 21:13:36 +0100

    Integrate reset function for database which may become an endpoint for cron based resets

--- src/persistence/fakeDBConnection.js
+++ src/persistence/fakeDBConnection.js
@@ -78,11 +78,13 @@ function provisionCreateEmployee() {
     };
 }
 
-function createFakeData() {
+function createFakeData(count) {
     log.info('fakeDB.createFakeData');
     if (employeeDB === undefined) {
         employeeDB = [];
-        for (var i = 0; i < config.persistence.fakeDB.employeeCount; i++) {
+    }
+    if (employeeDB.length === 0) {
+        for (var i = 0; i < count; i++) {
             var e = new Employee(provisionCreateEmployee());
             employeeDB.push(e);
         }
@@ -92,7 +94,7 @@ function createFakeData() {
     return employeeDB;
 }
 
-createFakeData();
+createFakeData(config.persistence.fakeDB.employeeCount);
 
 // **************************************************************************
 // Data Manipulation
@@ -187,6 +189,11 @@ function deleteJob(userName, jobId) {
         }
     }
 }
+
+exports.resetData = function (count) {
+    employeeDB = [];
+    createFakeData(count);
+};
 
 //TODO: create a object for database instead of module
 exports.getEmployeeByUserName = getEmployeeByUserName;
